3dc1734f447366802335ca184bcedd1e14bb1fb8
Aashish Reddy <37476786+Aashishreddy@users.noreply.github.com>
2022-02-05 00:18:31 -0600

    Validating Credentials Changes

--- src/ToDo/LoginComponent.jsx
+++ src/ToDo/LoginComponent.jsx
@@ -20,8 +20,10 @@ class LoginComponent extends Component{
                    Username: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /> <br />
                    Password: <input type="password" name="password" value = {this.state.password} onChange={this.handleChange}/> <br />
                    <button onClick={this.loginClicked}>Login</button> <br />
-                   <InvalidCredentials hasLoginFailed= {this.state.hasLoginFailed} />
-                   <ValidCredentials isSuccess= {this.state.showSuccessMessage} /> 
+                   {/* <InvalidCredentials hasLoginFailed= {this.state.hasLoginFailed} />
+                   <ValidCredentials isSuccess= {this.state.showSuccessMessage} /> */}
+                   {this.state.hasLoginFailed && <div>Invalid Credentials</div>} 
+                   {this.state.showSuccessMessage && <div>Valid Credentials</div>} 
                </div> 
         );
     }
@@ -61,19 +63,19 @@ class LoginComponent extends Component{
 
 }
 
-function InvalidCredentials(props){
-    if(props.hasLoginFailed){
-        return <div>Invalid Credentials</div>
-    }
-    return null       
-}
+// function InvalidCredentials(props){
+//     if(props.hasLoginFailed){
+//         return <div>Invalid Credentials</div>
+//     }
+//     return null       
+// }
 
-function ValidCredentials(props){
-    if(props.isSuccess){
-        return <div>Valid Credentials</div>
-    }
-    return null
-}
+// function ValidCredentials(props){
+//     if(props.isSuccess){
+//         return <div>Valid Credentials</div>
+//     }
+//     return null
+// }
 
 
   
