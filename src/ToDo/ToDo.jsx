8a144839002c8891162094ff0fbb3c985f9e674b
Aashish Reddy <37476786+Aashishreddy@users.noreply.github.com>
2022-02-05 01:10:55 -0600

    Routing

--- src/ToDo/ToDo.jsx
+++ src/ToDo/ToDo.jsx
@@ -1,14 +1,23 @@
 import react, {Component} from "react";
 // No Curly braces for imports
 import LoginComponent from "./LoginComponent";
+import WelcomeComponent from "./WelcomeComponent";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 
 class ToDo extends Component{
     render(){
         return (
             <div className='ToDo'>
-                ToDo Application
-                <LoginComponent />
+                {/* A Router can only have one child. Hence define it in a empty fragment. */}
+                <Router>
+                    <Routes>
+                        <Route path="/" element= {<LoginComponent />} />
+                        <Route path="/login" element= {<LoginComponent />} />
+                        <Route path="/welcome" element = {<WelcomeComponent />} />
+                    </Routes>
+                </Router>
+            
             </div>
         );
     }
---
+++ src/ToDo/WelcomeComponent.jsx
@@ -0,0 +1,11 @@
+import react, { Component } from "react";
+
+class WelcomeComponent extends Component{
+    render(){
+        return(
+             <div className="WelcomeComponent">A</div>   
+        );
+    }
+}
+
+export default WelcomeComponent
